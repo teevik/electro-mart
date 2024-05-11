56c2f66fbc3229e7b26b43ea4f2ad7fe8d887aff
teevik <teemu.vikoren@gmail.com>
2024-05-11 14:55:37 +0200

    Update image urls to use imgur

---
+++ api/migrations/20240511123307_update-image-urls.sql
@@ -0,0 +1,114 @@
+-- Update image urls to use imgur
+
+-- Products
+
+UPDATE `product`
+SET `image_url` = 'https://i.imgur.com/ctJaxBL.png'
+WHERE `name` = 'iPhone 12';
+
+UPDATE `product`
+SET `image_url` = 'https://i.imgur.com/3Ak2LNL.png'
+WHERE `name` = 'Galaxy S21';
+
+UPDATE `product`
+SET `image_url` = 'https://i.imgur.com/nHDM4kf.png'
+WHERE `name` = 'MatePad Pro';
+
+UPDATE `product`
+SET `image_url` = 'https://i.imgur.com/46IPKTn.png'
+WHERE `name` = 'Mi Notebook Pro';
+
+UPDATE `product`
+SET `image_url` = 'https://i.imgur.com/amT5P7N.png'
+WHERE `name` = 'OnePlus 9 Pro';
+
+UPDATE `product`
+SET `image_url` = 'https://i.imgur.com/WvLSYJ6.png'
+WHERE `name` = 'iPad Pro';
+
+UPDATE `product`
+SET `image_url` = 'https://i.imgur.com/PH9luyo.png'
+WHERE `name` = 'Galaxy Tab S7';
+
+UPDATE `product`
+SET `image_url` = 'https://i.imgur.com/eLsuIDy.png'
+WHERE `name` = 'MateBook X Pro';
+
+UPDATE `product`
+SET `image_url` = 'https://i.imgur.com/kTLqyte.png'
+WHERE `name` = 'Mi Gaming Laptop';
+
+UPDATE `product`
+SET `image_url` = 'https://i.imgur.com/gBXTpda.png'
+WHERE `name` = 'OnePlus 9';
+
+UPDATE `product`
+SET `image_url` = 'https://i.imgur.com/sA59nyB.png'
+WHERE `name` = 'MacBook Pro';
+
+UPDATE `product`
+SET `image_url` = 'https://i.imgur.com/4n22DQX.png'
+WHERE `name` = 'Galaxy Book Flex';
+
+UPDATE `product`
+SET `image_url` = 'https://i.imgur.com/asgQwr6.png'
+WHERE `name` = 'MateBook D';
+
+UPDATE `product`
+SET `image_url` = 'https://i.imgur.com/WXuZkq8.png'
+WHERE `name` = 'Mi Notebook Air';
+
+UPDATE `product`
+SET `image_url` = 'https://i.imgur.com/sRmSBu8.png'
+WHERE `name` = 'OnePlus 8T';
+
+UPDATE `product`
+SET `image_url` = 'https://i.imgur.com/gkFzXSr.png'
+WHERE `name` = 'iMac';
+
+UPDATE `product`
+SET `image_url` = 'https://i.imgur.com/7sNGJ0x.png'
+WHERE `name` = 'Galaxy Book S';
+
+-- Categories
+UPDATE `category`
+SET `image_url` = 'https://i.imgur.com/IgbfsvF.png'
+WHERE `name` = 'Smartphone';
+
+UPDATE `category`
+SET `image_url` = 'https://i.imgur.com/TLY5Ct6.png'
+WHERE `name` = 'Tablet';
+
+UPDATE `category`
+SET `image_url` = 'https://i.imgur.com/Cxbnoi2.png'
+WHERE `name` = 'Laptop';
+
+UPDATE `category`
+SET `image_url` = 'https://i.imgur.com/9HbHrkV.png'
+WHERE `name` = 'Desktop';
+
+UPDATE `category`
+SET `image_url` = 'https://i.imgur.com/I0fMAEa.png'
+WHERE `name` = 'Smartwatch';
+
+
+-- Brands
+UPDATE `brand`
+SET `image_url` = 'https://i.imgur.com/q7q7YjU.png'
+WHERE `name` = 'Apple';
+
+UPDATE `brand`
+SET `image_url` = 'https://i.imgur.com/jEQrqSe.png'
+WHERE `name` = 'Samsung';
+
+UPDATE `brand`
+SET `image_url` = 'https://i.imgur.com/0rslAcM.png'
+WHERE `name` = 'Huawei';
+
+UPDATE `brand`
+SET `image_url` = 'https://i.imgur.com/kPIF2UG.png'
+WHERE `name` = 'Xiaomi';
+
+UPDATE `brand`
+SET `image_url` = 'https://i.imgur.com/nTIlFp0.png'
+WHERE `name` = 'OnePlus';
